282b42357cf3e31f0c585e54ba847dd2122a2dde
azizbo <azizbourguiba.ab@gmail.com>
2019-11-18 23:05:09 +0100

    LocalStoragePanier

--- src/app/category-list/category-list.component.ts
+++ src/app/category-list/category-list.component.ts
@@ -4,23 +4,48 @@ import {CategoryService} from '../category.service';
 import {Router} from '@angular/router';
 import {Category} from '../category';
 
+interface MyObj {
+  idProduit: number;
+  nbProduit: number;
+}
+
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css']
 })
+
 export class CategoryListComponent implements OnInit {
-  categories;
   tab: [];
+  panier1: MyObj;
+  panier2: MyObj;
+  panier3: MyObj;
+  categories;
+  tabReq: MyObj[];
+  tabRes: MyObj[];
+  tabPanier;
+  allProductStringReq: string;
+  allProductStringRes: string;
+
   constructor(private categoryService: CategoryService, private router: Router) {
   }
 
   ngOnInit() {
     this.reloadData();
+    localStorage.clear();
+    this.allProductStringRes = localStorage.getItem('panierKey');
+    this.tabRes = JSON.parse(this.allProductStringRes);
+    console.log(this.tabRes) ;
+    // Function coming ***********************************************************
+  /*  this.allProductStringReq = JSON.stringify(this.tabReq);
+    localStorage.setItem('panierKey', this.allProductStringReq);*/
   }
 
   reloadData() {
-    this.categories = this.categoryService.getCategoriesList().subscribe( r => { this.tab = r ; console.log(this.tab)});
+    this.categories = this.categoryService.getCategoriesList().subscribe(r => {
+      this.tab = r;
+      console.log(this.tab) ;
+    });
 
 
   }
@@ -40,4 +65,20 @@ export class CategoryListComponent implements OnInit {
     this.reloadData();
     this.router.navigate(['updateCategory', id]);
   }
+
+  addPanierListe(id: number , qte: number) {
+if (this.tabRes !== null) {
+    for (let f of this.tabRes) {
+      if (f.idProduit === id) {
+        f.nbProduit = f.nbProduit + qte;
+      } else { this.panier3 = {idProduit: id , nbProduit: qte} ;
+               this.tabRes.push(this.panier3); break ;
+      }
+    }
+    this.tabRes = [] ;
+    this.tabRes.push(this.panier3 = {idProduit: id , nbProduit: qte});
+}
+  }
+
+
 }
