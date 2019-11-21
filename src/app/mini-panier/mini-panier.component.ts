08f6799069c2cd8cbeaa42097d3b19164e601503
ReserveHotelOnline <dr.kira1@hotmail.com>
2019-11-21 12:47:25 +0100

    done

--- src/app/mini-panier/mini-panier.component.ts
+++ src/app/mini-panier/mini-panier.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {style} from '@angular/animations';
 import {NavComponent} from '../nav/nav.component';
+interface CartProdcut {
+  name: string;
+  price: number;
+  qte: number;
+}
 
 @Component({
   selector: 'app-mini-panier',
@@ -8,13 +13,53 @@ import {NavComponent} from '../nav/nav.component';
   styleUrls: ['./mini-panier.component.css']
 })
 export class MiniPanierComponent implements OnInit {
+  private tabRes: CartProdcut[];
+  private allProductStringRes: string;
+  private qte: number;
 
-  constructor(private navComponent: NavComponent) { }
+  constructor(private navComponent: NavComponent) {
+  }
 
   ngOnInit() {
+    this.qte = 0;
+    this.allProductStringRes = localStorage.getItem('panierKey');
+    this.tabRes = JSON.parse(this.allProductStringRes);
+    console.log(this.tabRes);
+  }
+  recup() {
+
+    this.allProductStringRes = localStorage.getItem('panierKey');
+    this.tabRes = JSON.parse(this.allProductStringRes);
+    console.log(this.tabRes) ;
   }
 
   hide() {
+
     this.navComponent.hideShowPan(2);
   }
+
+  incr(n: string) {
+    for (const f of this.tabRes) {
+      if (f.name === n) {
+        f.qte++;
+        this.qte++;
+        break; }}
+    this.allProductStringRes = JSON.stringify(this.tabRes);
+    localStorage.setItem('panierKey', this.allProductStringRes);
+  }
+  suppProduct(n) {
+    this.allProductStringRes = localStorage.getItem('panierKey');
+    this.tabRes = JSON.parse(this.allProductStringRes);
+    console.log(this.tabRes) ;
+    let i = 0 ;
+    for (let f of this.tabRes) {
+      if (f.name === n) {
+        this.tabRes.splice(i, 1);
+      }
+      i++ ;
+    }
+    this.allProductStringRes = JSON.stringify(this.tabRes);
+    localStorage.setItem('panierKey', this.allProductStringRes);
+  }
+
 }
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -4,7 +4,11 @@ import {Product} from '../product';
 import {ProductService} from '../product.service';
 import {Router} from '@angular/router';
 import {Category} from '../category';
-
+interface CartProdcut {
+  name: string;
+  price: number;
+  qte: number;
+}
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -13,27 +17,65 @@ import {Category} from '../category';
 export class ProductListComponent implements OnInit {
   products: Product[];
   category: Category;
-  constructor(private productService: ProductService, private router: Router) { }
+  cart: CartProdcut;
+  private tabRes: CartProdcut[];
+  private allProductStringRes: string;
+  private test = true;
+
+  constructor(private productService: ProductService, private router: Router) {
+  }
 
   ngOnInit() {
     this.reloadData();
   }
+
   reloadData() {
+
+
     this.productService.getProducts().subscribe(r => {
       this.products = r;
     });
   }
+
   deleteProduct(idProduct: number) {
     this.productService.deleteProduct(idProduct).subscribe(data => {
       console.log(data);
       this.reloadData();
     }, error => console.log(error));
   }
+
   productDetails(id: number) {
     this.router.navigate(['detailProduct', id]);
   }
+
   updateProduct(id: number) {
     this.reloadData();
     this.router.navigate(['updateProduct', id]);
+  }
+
+  addToCart(n: string, p: number) {
+    this.allProductStringRes = localStorage.getItem('panierKey');
+    this.tabRes = JSON.parse(this.allProductStringRes);
+    console.log(this.tabRes) ;
+    if (this.tabRes != null || this.tabRes !== undefined || this.tabRes !== []) {
+      console.log('ni null ni undedf  ');
+      for (const f of this.tabRes) {
+        if (f.name === n) {
+          f.qte = f.qte + 1;
+          console.log('zedna quantité khater ' + f.name + ' === ' + n);
+          this.test = false;
+          break;
+        } else { this.test = true ; }}
+      if (this.test === true) {
+        this.cart = {name: n, price: p, qte: 1};
+        this.tabRes.push(this.cart);
+        console.log('zedna produit jdid khater ' ); }
+
+    } else {
+      this.tabRes = [] ;
+      this.tabRes.push(this.cart = {name: n, price: p, qte: 1}); console.log('tab jdida ');}
+    console.log(this.tabRes);
+    this.allProductStringRes = JSON.stringify(this.tabRes);
+    localStorage.setItem('panierKey', this.allProductStringRes);
   }
 }
